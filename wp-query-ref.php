9e911982440141a13cb1dd8ba1ad6b35cd7bbdd7
luetkemj <luetkemj@gmail.com>
2014-04-21 12:53:02 -0400

    Updated source URL in head to 3.9

--- wp-query-ref.php
+++ wp-query-ref.php
@@ -4,7 +4,7 @@
 * Compiled by luetkemj - luetkemj.com
 *
 * CODEX: http://codex.wordpress.org/Class_Reference/WP_Query#Parameters
-* Source: http://core.trac.wordpress.org/browser/tags/3.8.3/src/wp-includes/query.php
+* Source: https://core.trac.wordpress.org/browser/tags/3.9/src/wp-includes/query.php
 */
  
 $args = array( 
